d791609066f6453e828c2943d45619054b973791
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-03 01:12:57 +0200

    Tests für add und normalize implementiert.

--- test/PolyTest.hs
+++ test/PolyTest.hs
@@ -9,7 +9,7 @@ import Test.Tasty.HUnit
 Hier Testen wir die Funktionen aus Poly.hs mithilfe von HUnit 
 
 Mit defaultMain sagen wir, dass wir beim starten des Programms alle Tests ausführen wollen, 
-bzw. die Funktion "tests" ausführen wollen:l.
+bzw. die Funktion "tests" ausführen wollen.
 
 -}
 
@@ -17,6 +17,10 @@ main :: IO ()
 main = defaultMain tests
 
 {-
+
+Um die Tests auszuführen, muss in der Konsole eingegeben werden:
+cabal test --builddir=D:\HochschuleRheinMain\4.Semester\FP\cabal-build-fpi
+Weil die Tests in einem anderen Build-Ordner liegen, als das normale Projekt.
 
 testGroup ist eine Funktion, die eine Gruppe von Tests zusammenfasst (Liste von Tests).
 testCase ist eine Funktion, die einen einzelnen Testfall beschreibt.
@@ -30,7 +34,31 @@ vor dem $ steht der Name des Testfalls, nach dem $ steht der eigentliche Test, d
 tests :: TestTree
 tests =
   testGroup "Poly Tests"
-    [ testCase "normalize entfernt Nullmonom" $
+
+    [ 
+
+     {- Tests für Add: -}
+      testCase "addiert zwei Polynome mit unterschiedlichen Exponenten" $
+        add (P [M 2 3]) (P [M 4 1]) @?= P [M 2 3, M 4 1]
+
+    , testCase "addiert zwei Polynome mit gleichen Exponenten" $
+        add (P [M 2 3]) (P [M 4 3]) @?= P [M 6 3]
+
+    , testCase "addiert zwei Polynome mit gleichen Exponenten, bei der die Summe der Koeffizienten 0 ist" $
+        add (P [M 4 3]) (P [M (-4) 3]) @?= P []
+    
+    {- Tests für Normalize: -}
+    , testCase " entfernt Nullmonom" $
         normalize (P [M 0 2]) @?= P []
+
+    , testCase "entfernt Nullmonom aus mehreren Monomen" $
+        normalize (P [M 0 2, M 3 1, M 0 5]) @?= P [M 3 1]
+
+    , testCase "fasst Monome mit gleichem Exponenten zusammen" $
+        normalize (P [M 2 3, M 4 3]) @?= P [M 6 3]
+    
+    , testCase "fasst Monome mit gleichem Exponenten zusammen, bei der die Summe der Koeffizienten 0 ist" $
+        normalize (P [M 2 3 , M (-2) 3]) @?= P []
+
     ]
 
