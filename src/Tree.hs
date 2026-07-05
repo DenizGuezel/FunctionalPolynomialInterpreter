47a49eecab689cb8e57bfc67fafdb8d4d5e61162
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-05 17:57:14 +0200

    Funktion implementiert, die aus eine, Monom ein Baum macht.

--- src/Tree.hs
+++ src/Tree.hs
@@ -14,6 +14,8 @@ TVar: Variable, z.B ist TVar 2 ein Ausdrucksbaum, der die Variable x^2 repräsen
 TAdd: Addition, z.B. ist TAdd (TVar 2) (TConst (3 % 1)) ein Ausdrucksbaum, der die Addition von x^2 und 3 repräsentiert.
 TMul: Multiplikation, z.B. ist TMul (TVar 2) (TConst (3 % 1)) ein Ausdrucksbaum, der die Multiplikation von x^2 und 3 repräsentiert.
 
+Beispielsweise wäre ein Monom M 3 2, das 3x^2 repräsentiert, in einem Ausdrucksbaum als TMul (TConst (3 % 1)) (TVar 2) dargestellt.
+
 -}
 
 data ExprTree
@@ -22,3 +24,21 @@ data ExprTree
    | TAdd ExprTree ExprTree
    | TMul ExprTree ExprTree
    deriving (Show, Eq)
+
+
+{- 
+
+Diese Funktion soll ein Monom in einen Ausdrucksbaum umwandeln.
+Sie bekommt als Eingabe ein Monom, z.B. M 3 2 und gibt als Ausgabe einen Ausdrucksbaum zurück, z.B. TMul (TConst (3 % 1)) (TVar 2).
+
+Mithilfe von pattern matching wird das Monom in seine Bestandteile zerlegt, nämlich den Koeffizienten k und den Exponenten e.
+
+-}
+
+monomToExprTree :: Monom -> ExprTree
+monomToExprTree (M k 0) = TConst k
+monomToExprTree (M 1 e) = TVar e
+monomToExprTree (M 0 e) = TConst 0
+monomToExprTree (M k e) = TMul (TConst k) (TVar e)
+
+
