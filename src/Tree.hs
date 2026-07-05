c5283fd2a79aae25542f6d51b772dc69b138a90c
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-05 19:44:48 +0200

    Rekurisve Hilfsfunktion zur Darstellung implementiert.

--- src/Tree.hs
+++ src/Tree.hs
@@ -41,7 +41,6 @@ monomToExprTree (M 1 e) = TVar e
 monomToExprTree (M 0 e) = TConst 0
 monomToExprTree (M k e) = TMul (TConst k) (TVar e)
 
-
 {-
 
 Diese Funktion soll ein Polynom in einen Ausdrucksbaum umwandeln.
@@ -68,5 +67,36 @@ der den Ausdruck in einer lesbaren Form darstellt.
 prettyPrintExprTree :: ExprTree -> String 
 prettyPrintExprTree tree = prettyPrintExprTreeRec tree 0
 
+{- 
 
+Diese Hilfsfunktion wird rekursiv aufgerufen, um den Ausdrucksbaum in einen String umzuwandeln.
+Sie bekommt als Eingabe einen Ausdrucksbaum und eine Ebene (level), die angibt, wie tief wir uns im Baum befinden.
+
+replicate 4 ' ' erzeugt einen String, der aus 4 Leerzeichen besteht, also "    ". 
+
+Wenn als Parameter ein Baum übergeben wird, der eine Konstante besitzt, sprich nur einen Knoten, welcher der Root ist, 
+der aus einer Konstante besteht (z.B 4), dann würde replicate (0 * 2) ' ' ++ show 4 ++ "\n" ausgeführt werden, was "4\n" zurückgibt.
+Das Level ist hier 0, da wir uns auf der obersten Ebene befinden (Root).
+
+Wenn als Parameter ein Baum übergeben wird, der eine Variable besitzt, sprich nur einen Knoten, welcher der Root ist,
+der aus einer Variable besteht (z.B x^2), dann würde replicate (0 * 2) ' ' ++ "x^" ++ show 2 ++ "\n" ausgeführt werden, was "x^2\n" zurückgibt.
+
+Wenn als Parameter ein Baum übergeben wird, der eine Addition besitzt, sprich nur einen Knoten, welcher der Root ist,
+der aus einer Addition besteht (z.B x^2 + 3), dann würde replicate (0 * 2) ' ' ++ "+\n" ++ prettyPrintExprTreeRec left (0 + 1) ++ prettyPrintExprTreeRec right (0 + 1) 
+ausgeführt werden, was "+\n  x^2\n  3\n" zurückgibt.
+
+Wenn als Parameter ein Baum übergeben wird, der eine Multiplikation besitzt, sprich nur einen Knoten, welcher der Root ist,
+der aus einer Multiplikation besteht (z.B x^2 * 3), dann würde replicate (0 * 2) ' ' ++ "*\n" ++ prettyPrintExprTreeRec left (0 + 1) ++ prettyPrintExprTreeRec right (0 + 1)
+ausgeführt werden, was "*\n  x^2\n  3\n" zurückgibt.
+
+-}
+
+prettyPrintExprTreeRec :: ExprTree -> Int -> String
+prettyPrintExprTreeRec (TConst k) level = replicate (level * 2) ' ' ++ show k ++ "\n"
+prettyPrintExprTreeRec (TVar e) level = replicate (level * 2) ' ' ++ "x^" ++ show e ++ "\n"
+prettyPrintExprTreeRec (TAdd left right) level = 
+    replicate (level * 2) ' ' ++ "+\n" ++ prettyPrintExprTreeRec left (level + 1) ++ prettyPrintExprTreeRec right (level + 1)
+prettyPrintExprTreeRec (TMul left right) level = 
+    replicate (level * 2) ' ' ++ "*\n" ++ prettyPrintExprTreeRec left (level + 1) ++ prettyPrintExprTreeRec right (level + 1)
+    
 
