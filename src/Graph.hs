b09608f163b59d66dbb2273b457f184b030a17a0
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-07 00:17:16 +0200

    Grundstruktur aufgebaut.

---
+++ src/Graph.hs
@@ -0,0 +1,14 @@
+module Graph where
+
+import Poly
+import Format (prettyRational) 
+
+{- Dieses Modul dient dazu, einen Graphen zu visualisieren, sie enthält die ganze Logik. -}
+
+
+{-
+
+Diese Funktion berechnet für eine Liste von x-Werten die entsprechenden y-Werte für 
+ein gegebenes Polynom und gibt eine Liste von Tupeln zurück, die die Punkte darstellen.
+
+-}
