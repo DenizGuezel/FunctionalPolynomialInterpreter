129aa5980b1d027f3181d60eff30e90e9212e08e
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-07 00:24:12 +0200

    samplePoints zum Errechnen von y-Werten implementiert.

--- src/Graph.hs
+++ src/Graph.hs
@@ -11,4 +11,10 @@ import Format (prettyRational)
 Diese Funktion berechnet für eine Liste von x-Werten die entsprechenden y-Werte für 
 ein gegebenes Polynom und gibt eine Liste von Tupeln zurück, die die Punkte darstellen.
 
+Sie bekommt als Eingabe ein Polynom und eine Liste von x-Werten und gibt als Ausgabe eine Liste von 
+Tupeln zurück, die die Punkte darstellen, die auf dem Graphen des Polynoms liegen.
+
 -}
+
+samplePoints :: Poly -> [Rational] -> [(Rational, Rational)]
+samplePoints poly xs = map (\x -> (x, evaluate poly x)) xs
