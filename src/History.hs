9df96ed158930f0c46cc7bb65ee3a5951f107f51
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-07 13:51:38 +0200

    addHistory implementiert, die einen Eintag in eine History einfügt.

--- src/History.hs
+++ src/History.hs
@@ -17,9 +17,23 @@ Entry (PolyResult "f" (P [M 1 0, M 2 1]))
  (Entry (ValueResult "g" 3) 
   (Entry (DivResult "h" (P [M 1 0]) (P [M 2 0])) Empty))
 
+Am Ende kommt immer Empty, um das Ende der Historie zu markieren und weil das Empty bei einem neuen Eintrag zu dem 
+neuen Eintrag hinzugefügt wird, um die Historie zu erweitern.
+
 -}
 
 data History a = 
     Empty
     | Entry a (History a)
     deriving (Show, Eq)
+
+{- 
+
+Diese Funktion fügt einen neuen Eintrag in die Historie ein.
+Sie nimmt als Eingabe z.B ein bereits berechnetes Ergebnis (z.B. PolyResult, ValueResult oder DivResult) und 
+die aktuelle Historie und gibt eine neue Historie zurück, die den neuen Eintrag enthält.
+
+-}
+
+addHistory :: a -> History a -> History a
+addHistory input Empty = Entry input Empty
