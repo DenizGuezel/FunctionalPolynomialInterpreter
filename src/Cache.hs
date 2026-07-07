2e7f82bbcec6ce307fea5fa8ac51a7098eafa466
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-08 00:11:47 +0200

    lookUpCache implementiert, um anhand einer Operation die man sucht, den Namen zu finden.

--- src/Cache.hs
+++ src/Cache.hs
@@ -20,6 +20,8 @@ die angewendet werden können, um ein Ergebnis zu berechnen.
 
 z.B Normalize P [M 1 0, M 2 1] repräsentiert die Normalisierung des Polynoms P [M 1 0, M 2 1].
 
+Es wird nicht das Ergbenis einer Operation gespeichert, sondern die Operation selbst, welche ausgeführt werden soll.
+
 -}
 
 data Operation
@@ -37,6 +39,30 @@ data Operation
 Dieser Datentyp repräsentiert den Cache, der verschiedene Operationen als Paar mit einem String speichert. 
 z.B [(Normalize (P [M 1 0, M 2 1]), "f"), (Negate (P [M 1 0, M 2 1]), "g")]. 
 
+Bei diesem Datentyp arbeiten wir mit type, da wir nur einen neuen Namen für bereits existierende 
+Datentypen erstellen wollen und keine neuen Konstruktoren oder Funktionen benötigen.
+
 -}
 
 type Cache = [(Operation, String)]
+
+{- 
+
+Diese Funktion sucht nach einer Operation in der Cache und gibt den zugehörigen Namen zurück.
+
+Sie nimmt als Eingabe eine Operation und eine Cache (Liste von Operationen und Namen) und gibt, wenn die Operation gefunden wurde, 
+den zugehörigen Namen zurück, wenn nicht, dann Nothing.
+
+Wenn der Cache leer ist, wird Nothing zurückgegeben. 
+Wenn ein Cache mit mindestens einer Operation vorhanden ist, wird die erste Operation überprüft und wenn sie mit der gesuchten Operation 
+übereinstimmt, wird der zugehörige Name zurückgegeben. Wenn der erste Eintrag nicht mit der gesuchten Operation übereinstimmt, 
+wird die Funktion rekursiv auf die restliche Cache angewendet, um die restlichen Einträge zu überprüfen.
+
+-}
+
+lookupCache :: Operation -> Cache -> Maybe String
+lookupCache op [] = Nothing
+lookupCache op ((cachedOp, name):rest) =
+    if op == cachedOp
+        then Just name
+        else lookupCache op rest
