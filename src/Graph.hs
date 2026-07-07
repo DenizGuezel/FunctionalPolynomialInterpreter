5b3eca5c971be92529920a78782602e8e6b1868b
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-07 10:46:22 +0200

    Methode implementiert, die den Graph üblich auf diesem Bereich erzeugt.

--- src/Graph.hs
+++ src/Graph.hs
@@ -25,14 +25,14 @@ samplePoints poly xs = map (\x -> (x, evaluate poly x)) xs
 Diese Funktion berechnet für ein gegebenes Polynom eine unendliche Liste von Punkten, die auf dem Graphen des Polynoms liegen.
 Sie bekommt als Eingabe ein Polynom und gibt als Ausgabe eine unendliche Liste von Tupeln zurück, die die Punkte darstellen, die auf dem Graphen des Polynoms liegen.
 
-x ist hierbei eine unendliche Liste von Rationalen Zahlen, die von -100 bis unendlich geht.
+x ist hierbei eine unendliche Liste von Rationalen Zahlen, die von -10 bis unendlich geht mithilfe von Lazy Evaluation.
 
 Sinn dieser Funktion ist es, die Punkte des Graphen des Polynoms zu berechnen, um sie in der GUI anzuzeigen.
 
 -}
 
 infinitePoints :: Poly -> [(Rational, Rational)]
-infinitePoints poly = [(x, evaluate poly x) | x <- [(-100)..]]
+infinitePoints poly = [(x, evaluate poly x) | x <- [(-10)..]]
 
 {- 
 
@@ -43,3 +43,14 @@ Sie nimmt mithilfe von take n Punkte aus der unendlichen Liste von Punkten, die 
 
 visiblePoints :: Int -> Poly -> [(Rational, Rational)]
 visiblePoints n poly = take n (infinitePoints poly)
+
+
+{- 
+
+Diese Funktion erzeugt einen Standardausschnitt von Punkten für ein gegebenes Polynom, um den Graphen in der GUI anzuzeigen.
+Standard ist hier dann [-10 .. 10] für x-Werte und die entsprechenden y-Werte werden berechnet.
+
+-}
+
+defaultPoints :: Poly -> [(Rational, Rational)]
+defaultPoints poly = visiblePoints 21 poly 
