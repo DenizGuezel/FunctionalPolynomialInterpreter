77cfa70220d93a249f501dab92c73747bf9e3c35
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-08 14:56:12 +0200

    Funktion zum evaluieren von mehreren Polynomen hinzugefügt.

--- src/Parallel.hs
+++ src/Parallel.hs
@@ -4,4 +4,13 @@ import Poly
 
 {- Dieses Modul dient dazu, mehrere Polynome gleichzeitig zu verarbeiten -}
 
+{- 
 
+Diese Funktion dient dazu, mehrere Polynome gleichzeitig zu evaluieren. 
+Sie nimmt einen Wert x und eine Liste von Polynomen und gibt eine Liste von Rationalen zurück, 
+die die Ergebnisse der Evaluierung jedes Polynoms an der Stelle x enthalten.
+
+-}
+
+evaluateMany :: Rational -> [Poly] -> [Rational]
+evaluateMany x polys = map (\poly -> evaluate poly x) polys
