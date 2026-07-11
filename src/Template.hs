cd2674ea7310f4931b9cd6157e3e4d7c09a7683d
DenizGuezel <deniz.guezel@student.hs-rm.de>
2026-07-11 15:05:31 +0200

    Funktion erstellt, die einen Ausdurck erzeugt, welcher einen Polynom repräsentiert.

--- src/Template.hs
+++ src/Template.hs
@@ -5,4 +5,15 @@ module Template where
 import Poly
 import Language.Haskell.TH
 
+{- Dieses Modul dient dazu, die Funktionen zu definieren, die zur Compile-Zeit Haskell-Code erzeugen-}
 
+{- 
+
+Diese Funktion erzeugt einen Ausdruck, der ein Polynom darstellt, das aus einer Liste von Monomen besteht. 
+Jedes Monom wird durch ein Tupel (k, e) repräsentiert, wobei k der Koeffizient und e der Exponent ist. 
+Die Funktion verwendet Template Haskell, um den entsprechenden Haskell-Code zur Compile-Zeit zu generieren.
+
+-}
+
+polyExp :: [(Integer, Int)] -> Q Exp
+polyExp monoms = [| P [M (fromInteger k) e | (k, e) <- monoms] |]
